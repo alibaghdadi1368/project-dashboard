fea72bc8b883f3b81afe46cf208726db700df760
Ali Baghdadi <alibaghdadi1368@gmail.com>
2024-06-21 13:55:19 -0700

    Update run.py to get json file

--- src/run.py
+++ src/run.py
@@ -1,4 +1,21 @@
 import streamlit as st
+import pandas as pd
+from io import StringIO
+import  json
 
 st.title(':zap: Streamlit Project Dashboard')
 st.text('This is my first public Git repository!')
+
+with st.expander('Statistics'):
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        # To read file as bytes:
+        # To convert to a string based IO:
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+
+        # To read file as string:
+        string_data = stringio.read()
+        st.write(string_data)
+
+        data = json.loads(string_data)
+        st.json(data)
