fa6fa1d2ca951f55ba6ca05612f64f2bebea8f16
Ali Baghdadi <alibaghdadi1368@gmail.com>
2024-06-30 02:22:38 -0700

    Update user landing page - Add login/signup page - Add user profile info - Add statistics

--- src/run.py
+++ src/run.py
@@ -1,21 +1,53 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from PIL import Image
 from io import StringIO
 import  json
 
+# Login
+login_option = st.sidebar.radio('Login/Signup', ('Login', 'Signup'))
+
+if login_option == 'Login':
+    with st.sidebar.form("Login"):
+        st.write("Login Here.")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+
+   # Every form must have a submit button.
+        submitted = st.form_submit_button("Login")
+        if submitted:
+            pass
+else:
+    with st.sidebar.form("Signup"):
+        st.write("Signup Here.")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+        email = st.text_input("Email")
+
+   # Every form must have a submit button.
+        submitted = st.form_submit_button("Signup")
+        if submitted:
+            pass
+
+
+# Banner
+banner = Image.open('./data/python_banner.jpg')
+st.image(banner)
 st.title(':zap: Streamlit Project Dashboard')
-st.text('This is my first public Git repository!')
 
+
+# Statistics
 with st.expander('Statistics'):
-    uploaded_file = st.file_uploader("Choose a file")
-    if uploaded_file is not None:
-        # To read file as bytes:
-        # To convert to a string based IO:
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+    sns.histplot(np.random.randn(100), ax=ax)
+    st.pyplot(fig)
 
-        # To read file as string:
-        string_data = stringio.read()
-        st.write(string_data)
-
-        data = json.loads(string_data)
-        st.json(data)
+# User Info
+with st.expander('User Profile'):
+    col1, col2 = st.columns(2)
+    col1.text_input('Name:')
+    col2.text_input('Location:')
+    st.camera_input('Camera Input', key='camera_input')
