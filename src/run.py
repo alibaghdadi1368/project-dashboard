8bc41fb34bbdb305554375514fb30dfef1224c7b
Ali Baghdadi <alibaghdadi1368@gmail.com>
2024-07-01 05:06:38 -0700

    Add script to populate db from json data

--- src/run.py
+++ src/run.py
@@ -1,37 +1,44 @@
-import streamlit as st
-import pandas as pd
+import json
+
+import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt
+import streamlit as st
 from PIL import Image
-from io import StringIO
-import  json
+import os
+import re
+
+import sys
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+from db.models import Message
+
 
 # Login
-login_option = st.sidebar.radio('Login/Signup', ('Login', 'Signup'))
-
+login_option = st.sidebar.radio('Login/Singup', ('Login', 'Singup'))
 if login_option == 'Login':
-    with st.sidebar.form("Login"):
+    with st.sidebar.form("login"):
         st.write("Login Here.")
         username = st.text_input("Username")
         password = st.text_input("Password", type="password")
 
-   # Every form must have a submit button.
+        # Every form must have a submit button.
         submitted = st.form_submit_button("Login")
         if submitted:
             pass
 else:
-    with st.sidebar.form("Signup"):
-        st.write("Signup Here.")
+    with st.sidebar.form("signup"):
+        st.write("Singup Here.")
         username = st.text_input("Username")
         password = st.text_input("Password", type="password")
         email = st.text_input("Email")
 
-   # Every form must have a submit button.
-        submitted = st.form_submit_button("Signup")
+        # Every form must have a submit button.
+        submitted = st.form_submit_button("Singup")
         if submitted:
             pass
-
 
 # Banner
 banner = Image.open('./data/python_banner.jpg')
@@ -39,15 +46,22 @@ st.image(banner)
 st.title(':zap: Streamlit Project Dashboard')
 
 
-# Statistics
-with st.expander('Statistics'):
-    fig, ax = plt.subplots(1, 1, figsize=(10, 5))
-    sns.histplot(np.random.randn(100), ax=ax)
-    st.pyplot(fig)
+# Questions
+with st.expander('Q / A'):
+    query = st.text_input('Search:')
 
-# User Info
-with st.expander('User Profile'):
+    # select top 10 from messages
+    for msg in Message.objects.all().order_by('-date')[:20]:
+        if not msg.text or msg.text[-1] not in '؟?':
+            continue
+
+        if query and query not in msg.text:
+            continue
+
+        col1, col2 = st.columns([1, 4])
+        col1.write(f'**{msg.user.username}**')
+        col2.write(msg.text.replace(query, f'{query}'))
+
     col1, col2 = st.columns(2)
-    col1.text_input('Name:')
-    col2.text_input('Location:')
-    st.camera_input('Camera Input', key='camera_input')
+    col1.button('< Previous')
+    col2.button('Next >')
